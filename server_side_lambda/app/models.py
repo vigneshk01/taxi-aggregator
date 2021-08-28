97e959870e551974ce16f2ac4aebefbdbad8a584
Vignesh Kumar <33518968+vigneshk01@users.noreply.github.com>
2021-08-29 01:15:49 +0530

    server_side_lambda
    version_3

--- server_side_lambda/app/models.py
+++ server_side_lambda/app/models.py
@@ -1,8 +1,10 @@
 import datetime
+import pprint
 from datetime import timedelta
+
+from bson import SON
 from bson.json_util import dumps
 from flask import Flask, request
-
 from database import Database
 
 app = Flask(__name__)
@@ -11,9 +13,10 @@ db = Database()
 TAXI_COLLECTION = 'taxi'
 LOCATION_COLLECTION = 'boundary'
 RIDE_DETAILS_COLLECTION = 'ridedetails'
-STREAM_COLLECTION = 'taxi-location-stream'
+STREAM_COLLECTION = 'taxi_location_stream'
 SPECIAL_EVENTS_COLLECTION = 'special_events'
 USERS_COLLECTION = 'users'
+BOOKING_HISTORY_COLLECTION = 'booking_request_history'
 
 
 @app.route("/", methods=['GET'])
@@ -22,18 +25,15 @@ def home():
 
 
 @app.route("/taxi", methods=['GET'])
-def get_taxi_list():
-    taxi_data = db.get_all_data(TAXI_COLLECTION, {}, {'_id': 0})
+def get_taxi_list():  # will get called every minute
+    timestamp = datetime.datetime.now()
+    curr_time = timestamp.time().strftime("%H:%M")
+    end_time = (timestamp + timedelta(minutes=1)).strftime("%H:%M")
+    taxi_data = db.get_all_data(TAXI_COLLECTION, {'Shift_Start_Time': {'$gte': curr_time, '$lt': end_time}},
+                                {'_id': 0, 'TaxiID': 1})
     list_cur = list(taxi_data)
     json_data = dumps(list_cur)
     return json_data
-
-
-@app.route("/taxi/shiftCheck", methods=['GET'])
-def shift_checker():
-    shift_start = db.get_all_data(TAXI_COLLECTION, {'Shift_Start_Time': "06:00"}, {'_id': 0})
-    shift_end = db.get_all_data(TAXI_COLLECTION, {'shift_end_time': "18:00"}, {'_id': 0})
-    return
 
 
 @app.route("/taxi/specialEvent", methods=['GET'])
@@ -83,9 +83,23 @@ def migrate_taxi():
     return
 
 
-@app.route("/bookTaxi", methods=['GET'])
+@app.route("/bookTaxi", methods=['POST'])
 def book_taxi():
-    return
+    data = request.json
+    location = data['current_location']
+    range_query = {'current_location': SON([("$near", location), ("$maxDistance", 1000)])}
+    coll_documents = db.get_all_data(STREAM_COLLECTION, range_query, {'_id': 0, 'current_location': 1, 'taxiID': 1,
+                                                                      'taxi_type': 1})
+    list_coll = list(coll_documents)
+    if len(list_coll) > 0:
+        json_data = dumps(list_coll)
+        data['msg'] = 'cabs_available'
+        db.insert_single_data(BOOKING_HISTORY_COLLECTION, data)
+        return json_data
+    else:
+        data['msg'] = 'no_nearby_cabs'
+        db.insert_single_data(BOOKING_HISTORY_COLLECTION, data)
+        return None
 
 
 @app.route("/bookTaxi/{ID}", methods=['GET'])
