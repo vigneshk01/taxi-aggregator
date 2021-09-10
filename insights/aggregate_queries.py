016401e082e1aaab43f53a27778e9a9a3259e4ec
Vignesh Kumar <33518968+vigneshk01@users.noreply.github.com>
2021-09-10 08:12:49 +0530

    ver 2

--- insights/aggregate_queries.py
+++ insights/aggregate_queries.py
@@ -4,9 +4,9 @@ vehicle_daily_rating_avg = [
     }, {
         '$group': {
             '_id': {
-                'date': '$booked_time',
+                'date': '$booked_time', 
                 'vehicle_num': '$vehicle_num'
-            },
+            }, 
             'avg': {
                 '$avg': {
                     '$toInt': '$passenger_rating'
@@ -15,57 +15,91 @@ vehicle_daily_rating_avg = [
         }
     }, {
         '$project': {
-            '_id': 0,
-            'date': '$_id.date',
-            'vehicle_num': '$_id.vehicle_num',
+            '_id': 0, 
+            'date': '$_id.date', 
+            'vehicle_num': '$_id.vehicle_num', 
             'avg': '$avg'
         }
     }
 ]
 
-taxi_daily_projections =[
+hourly_booking_Aggr = [
     {
-        '$addFields': {
-            '1': {
-                '$toDate': '$booked_time'
-            },
-            '2': {
-                '$toDate': '$end_time'
-            },
-            '3': {
-                '$divide': [
-                    {
-                        '$subtract': [
-                            {
-                                '$toDate': '$end_time'
-                            }, {
-                                '$toDate': '$booked_time'
-                            }
-                        ]
-                    }, 60000
-                ]
+        '$match': {
+            'booked_time': {
+                '$gte': '2021-09-05T00:00:00.000', 
+                '$lt': '2021-09-06T00:00:00.000'
             }
         }
     }, {
         '$group': {
             '_id': {
-                'vehicle_num': '$vehicle_num',
+                'date': {
+                    '$dateFromParts': {
+                        'year': {
+                            '$year': {
+                                '$toDate': '$booked_time'
+                            }
+                        }, 
+                        'month': {
+                            '$month': {
+                                '$toDate': '$booked_time'
+                            }
+                        }, 
+                        'day': {
+                            '$dayOfMonth': {
+                                '$toDate': '$booked_time'
+                            }
+                        }, 
+                        'hour': {
+                            '$hour': {
+                                '$toDate': '$booked_time'
+                            }
+                        }
+                    }
+                }
+            }, 
+            'hourly_ride_count': {
+                '$sum': 1
+            }, 
+            'vehicle_num': {
+                '$addToSet': '$vehicle_num'
+            }
+        }
+    }, {
+        '$sort': {
+            '_id.date': 1
+        }
+    }, {
+        '$project': {
+            'date': '$_id.date', 
+            'hourly_ride_count': '$hourly_ride_count', 
+            'vehicle_num': '$vehicle_num'
+        }
+    }
+]
+
+taxi_daily_projections = [
+    {
+        '$group': {
+            '_id': {
+                'vehicle_num': '$vehicle_num', 
                 'date': {
                     '$dateToString': {
-                        'format': '%Y-%m-%d',
+                        'format': '%Y-%m-%d', 
                         'date': {
                             '$toDate': '$booked_time'
                         }
                     }
                 }
-            },
+            }, 
             'total_rides': {
                 '$sum': 1
-            },
+            }, 
             'income': {
                 '$sum': '$cost'
-            },
-            'total_ridein_mins': {
+            }, 
+            'total_ridetime_in_mins': {
                 '$sum': {
                     '$divide': [
                         {
@@ -83,12 +117,12 @@ taxi_daily_projections =[
         }
     }, {
         '$project': {
-            '_id': 0,
-            'vehicle_num': '$_id.vehicle_num',
-            'date': '$_id.date',
-            'total_rides': '$total_rides',
-            'income': '$income',
-            'total_ridein mins': '$total_ridein_mins'
+            '_id': 0, 
+            'vehicle_num': '$_id.vehicle_num', 
+            'date': '$_id.date', 
+            'total_rides': '$total_rides', 
+            'income': '$income', 
+            'total_ridetime_in_mins': '$total_ridetime_in_mins'
         }
     }
 ]
