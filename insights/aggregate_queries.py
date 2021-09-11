ccf74cd2b8f6e332bc398399e914f7dbb5ea70a5
Vignesh Kumar <33518968+vigneshk01@users.noreply.github.com>
2021-09-11 09:22:00 +0530

    added plots

--- insights/aggregate_queries.py
+++ insights/aggregate_queries.py
@@ -1,36 +1,6 @@
-vehicle_daily_rating_avg = [
+Dagg_passenger_rating = [
     {
         '$match': {}
-    }, {
-        '$group': {
-            '_id': {
-                'date': '$booked_time', 
-                'vehicle_num': '$vehicle_num'
-            }, 
-            'avg': {
-                '$avg': {
-                    '$toInt': '$passenger_rating'
-                }
-            }
-        }
-    }, {
-        '$project': {
-            '_id': 0, 
-            'date': '$_id.date', 
-            'vehicle_num': '$_id.vehicle_num', 
-            'avg': '$avg'
-        }
-    }
-]
-
-hourly_booking_Aggr = [
-    {
-        '$match': {
-            'booked_time': {
-                '$gte': '2021-09-05T00:00:00.000', 
-                '$lt': '2021-09-06T00:00:00.000'
-            }
-        }
     }, {
         '$group': {
             '_id': {
@@ -40,65 +10,61 @@ hourly_booking_Aggr = [
                             '$year': {
                                 '$toDate': '$booked_time'
                             }
-                        }, 
+                        },
                         'month': {
                             '$month': {
                                 '$toDate': '$booked_time'
                             }
-                        }, 
+                        },
                         'day': {
                             '$dayOfMonth': {
                                 '$toDate': '$booked_time'
                             }
-                        }, 
-                        'hour': {
-                            '$hour': {
-                                '$toDate': '$booked_time'
-                            }
                         }
                     }
+                },
+                'vehicle_num': '$vehicle_num'
+            },
+            'avg': {
+                '$avg': {
+                    '$toInt': '$passenger_rating'
                 }
-            }, 
-            'hourly_ride_count': {
-                '$sum': 1
-            }, 
-            'vehicle_num': {
-                '$addToSet': '$vehicle_num'
             }
         }
     }, {
-        '$sort': {
-            '_id.date': 1
+        '$project': {
+            '_id': 0,
+            'date': '$_id.date',
+            'vehicle_num': '$_id.vehicle_num',
+            'avg': '$avg'
         }
     }, {
-        '$project': {
-            'date': '$_id.date', 
-            'hourly_ride_count': '$hourly_ride_count', 
-            'vehicle_num': '$vehicle_num'
+        '$merge': {
+            'into': 'Dagg_passenger_rating'
         }
     }
 ]
 
-taxi_daily_projections = [
+Dagg_vehicle_stats = [
     {
         '$group': {
             '_id': {
-                'vehicle_num': '$vehicle_num', 
+                'vehicle_num': '$vehicle_num',
                 'date': {
                     '$dateToString': {
-                        'format': '%Y-%m-%d', 
+                        'format': '%Y-%m-%d',
                         'date': {
                             '$toDate': '$booked_time'
                         }
                     }
                 }
-            }, 
+            },
             'total_rides': {
                 '$sum': 1
-            }, 
+            },
             'income': {
                 '$sum': '$cost'
-            }, 
+            },
             'total_ridetime_in_mins': {
                 '$sum': {
                     '$divide': [
@@ -113,16 +79,88 @@ taxi_daily_projections = [
                         }, 60000
                     ]
                 }
+            },
+            'total_distance_covered_kms': {
+                '$sum': {
+                    '$add': {
+                        '$toInt': {
+                            '$arrayElemAt': [
+                                '$total_distance_kms', 0
+                            ]
+                        }
+                    }
+                }
             }
         }
     }, {
         '$project': {
-            '_id': 0, 
-            'vehicle_num': '$_id.vehicle_num', 
-            'date': '$_id.date', 
-            'total_rides': '$total_rides', 
-            'income': '$income', 
-            'total_ridetime_in_mins': '$total_ridetime_in_mins'
+            '_id': 0,
+            'vehicle_num': '$_id.vehicle_num',
+            'date': '$_id.date',
+            'total_rides': '$total_rides',
+            'income': '$income',
+            'total_ridetime_in_mins': '$total_ridetime_in_mins',
+            'total_distance_covered_kms': '$total_distance_covered_kms'
+        }
+    }, {
+        '$merge': {
+            'into': 'Dagg_vehicle_stats'
+        }
+    }
+]
+
+Hagg_rides_stats = [
+    {
+        '$match': {}
+    }, {
+        '$group': {
+            '_id': {
+                'date': {
+                    '$dateFromParts': {
+                        'year': {
+                            '$year': {
+                                '$toDate': '$booked_time'
+                            }
+                        },
+                        'month': {
+                            '$month': {
+                                '$toDate': '$booked_time'
+                            }
+                        },
+                        'day': {
+                            '$dayOfMonth': {
+                                '$toDate': '$booked_time'
+                            }
+                        },
+                        'hour': {
+                            '$hour': {
+                                '$toDate': '$booked_time'
+                            }
+                        }
+                    }
+                }
+            },
+            'hourly_ride_count': {
+                '$sum': 1
+            },
+            'vehicle_num': {
+                '$addToSet': '$vehicle_num'
+            }
+        }
+    }, {
+        '$sort': {
+            '_id.date': 1
+        }
+    }, {
+        '$project': {
+            '_id': 0,
+            'date': '$_id.date',
+            'hourly_ride_count': '$hourly_ride_count',
+            'vehicle_num': '$vehicle_num'
+        }
+    }, {
+        '$merge': {
+            'into': 'Hagg_rides_stats'
         }
     }
 ]
