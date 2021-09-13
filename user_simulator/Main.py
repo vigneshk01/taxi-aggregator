9b2603d9ec9de968a26b3e124d7ce40765b3b545
NischGit <nischishetty@gmail.com>
2021-09-13 17:01:26 +0530

    enhanced the usersimulator
    enhanced the UI simulator to call start ride, get location distance

--- user_simulator/Main.py
+++ user_simulator/Main.py
@@ -1,25 +1,19 @@
 from UserSimulation import User
 import time
+import json
 
 
 
 class UserSimulatorClient:
-    print('''===================
-            Sign up Process
-            ====================   
-    ''')
 
-    first_name = input("First Name: ")
-    last_name = input("Last Name: ")
 
-    Apikey = User().passenger_signup(first_name, last_name)
 
-    if (Apikey != ""):
-        print("Login Successful:")
 
-    else:
-        print("Invalid Login. Try again:")
-
+    print('''
+        =======================
+            Confirm Scenario
+        =======================   
+        ''')
     Scenario_Options = ["Regular", "Rush Hour", "Special Event"]
 
     # Print out your options
@@ -28,29 +22,42 @@ class UserSimulatorClient:
     result = "Noresult"
     # Take user input and get the corresponding item from the list
     while (result == "Noresult"):
-        inp = int(input("Enter scenario number: "))
+        inp = int(input("Enter scenario option number: "))
         if inp in range(1, 4):
             inp = Scenario_Options[inp - 1]
             result = "Pass"
         else:
             print("Invalid input. Try again!")
     if (inp == "Regular"):
+        print('''
+                ===================
+                      Sign in 
+                ====================   
+                ''')
         print("Enter Login Details:")
         Apikey=""
         while(Apikey==""):
-            user_id= input("UserName: ")
+            user_id = input("UserName: ")
             password = input("Password: ")
-            Apikey = User().passenger_login(user_id,password)
+            res = User().passenger_login(user_id,password)
+            print(res)
 
-            if (Apikey !=""):
-                print("Login Successful:")
-
-            else:
+            if res ==None:
                 print("Invalid Login. Try again:")
+            else:
+                print("Login Successful:")
+                res_dict = json.loads(res)
+                Apikey = res_dict['APIKey']
 
+
+        print('''
+                        =======================
+                         Initiate Taxi Booking
+                        =======================   
+                        ''')
         Origin = input("Enter Origin address: ")
         Destination = input("Enter Destination address: ")
-        taxi_type_options = ["Utility", "Deluxe", "Luxury", "All"]
+        taxi_type_options = ["UTILITY", "DELUXE", "LUXURY", "ALL"]
 
         # Print out your options
         for i in range(len(taxi_type_options)):
@@ -58,19 +65,69 @@ class UserSimulatorClient:
         result = "Noresult"
         # Take user input and get the corresponding item from the list
         while (result == "Noresult"):
-            inp = int(input("Enter Taxi Type number: "))
-            if inp in range(1, 5):
-                inp = taxi_type_options[inp - 1]
-                res = User().book_taxi( user_id, Origin, Destination, inp)
-                print(res)
-                if (res != ""):
-                    print("Login Successful:")
+            taxi_type = int(input("Enter Taxi Type number: "))
+            if taxi_type in range(1, 5):
+                taxi_type = taxi_type_options[taxi_type - 1]
+                res = User().book_taxi( user_id, Origin, Destination, taxi_type)
 
-                else:
-                    print("Invalid Login. Try again:")
-                result = "Pass"
+                print(res_dict)
+                if res == None:
+                    print('Sorry. No rides available at this moment.Please try again later')
+                    exit()
+                else :
+                    res_dict = json.loads(res)
+                    print(res_dict)
+                    print('Please find the cost for closest available taxi')
+                    print(' '.join(["Taxi type: "+res_dict['vehicle_type'], "Cost: "+res_dict['cost']]))
+
+                #confirm_booking= input("Please confirm your booking with yes/no:")
+
+                    taxi_num = res_dict['vehicle_num']
+                    res = User().book_taxi_confirm(user_id, taxi_num, Origin, Destination, taxi_type, Apikey)
+                    if (res != None):
+                        print(res)
+                        res_dict = json.loads(res)
+                        OTP = res_dict["OTP"]
+                        result == "Pass"
+                        print(f'Please note:Your OTP to start the ride is  {OTP}')
+                        print('''
+                                                   ============================================
+                                                              Checking taxi location
+                                                   ============================================   
+                                                   ''')
+
+                        countdown =1
+                        check_taxi =1
+                        while check_taxi:
+                            while countdown:
+                                mins, secs = divmod(10, 60)
+                                timer = '{:02d}:{:02d}'.format(mins, secs)
+                                print(timer, end="\r")
+                                time.sleep(10)
+                                countdown -= 1
+                                if countdown == 0:
+                                    res,distance = User().get_taxi_curr_location(Origin, OTP, taxi_num)
+                                    print(res)
+                                    print(f'your driver is in {res} and is {format(distance, ".2f")} Km(s) away from your location')
+
+                                    if (distance <= 0.5):
+                                        start_ride = 1
+                                        check_taxi = 0
+                                        while(start_ride):
+                                            OTP = input("Your ride has arrived.Please provide your OTP: ")
+                                            res = User().start_ride(OTP, taxi_num, Origin, Destination, Apikey)
+                                            print(res)
+                                            if (res != None):
+                                                print("Your ride has started. You can track your ride now")
+                                                start_ride = 0
+
+                                            else:
+                                                print("Invalid OTP. Try again:")
+                                    else:
+                                        countdown = 1
+                    else:
+                        print("Invalid Login. Try again:")
+
             else:
                 print("Invalid input. Try again!")
-
-
 
--- user_simulator/UserSimulation.py
+++ user_simulator/UserSimulation.py
@@ -3,7 +3,7 @@ import json
 from datetime import datetime
 import requests
 import hashlib
-
+from geopy.distance import geodesic
 
 class User:
 
@@ -13,7 +13,7 @@ class User:
         self._base_url = 'https://fep34ikk65.execute-api.us-east-1.amazonaws.com/dev'
         self._headers = {'Content-Type': 'application/json'}
         self._otp = ''
-
+        self.GApiKey= 'AIzaSyDyRi7MuGjrLBWt32v_T1U4DEeTAjdjnr8'
     @property
     def latest_error(self):
         print(self._latest_error)
@@ -26,16 +26,16 @@ class User:
 
     def passenger_signup(self,FirstName, LastName):
         path = "/api/users/newuser"
-        Concat_username = FirstName[0:3]+LastName[0:3]+"username"
+        Concat_username = FirstName[0:3]+LastName[0:3] + "username"
         Concat_password = FirstName[0:3] + LastName[0:3] + "password"
         print(Concat_username)
         print(Concat_password)
         data = {
             "firstname": FirstName,
             "lastname": LastName,
-            "password": Concat_password,
+            "password": str(hashlib.sha256(Concat_username.encode("utf-8") ).hexdigest()),
             "user_type": "Passenger",
-            "username": Concat_username
+            "username": str(hashlib.sha256(Concat_password.encode("utf-8") ).hexdigest())
 
 
         }
@@ -46,23 +46,24 @@ class User:
         print(res.headers)
         print(res.content)
         print(res)
-        if res == "<Response [400]>":
-            print(f'response - {res}')
-            return {res}
+        if res.status_code==200:
+
+            return "SignUp Successful"
         else:
-            print("Err")
-        return None
+            return "SignUp Failed"
+
 
     def passenger_login(self, user_id, Password):
         path = "/api/users/login"
-        username_hash = hashlib.sha256(user_id.encode())
-        password_hash = hashlib.sha256(Password.encode())
+        username_hash = hashlib.sha256( user_id.encode("utf-8") ).hexdigest()
+        password_hash = hashlib.sha256( Password.encode("utf-8") ).hexdigest()
         print(str(username_hash))
         print(str(password_hash))
         data = {
+            "username": str(username_hash),
             "password": str(password_hash),
-            "user_type": "Passenger",
-            "username": str(username_hash)
+            "user_type": "Passenger"
+
 
         }
 
@@ -72,93 +73,139 @@ class User:
         print(res.headers)
         print(res.content)
         if res.status_code == 200:
-            print(f'response - {res.content}')
-            return {res.content}
-        else:
-            print('Error while retrieving data')
-        return None
-
-    def get_lat_long(self, location):
-        location = location.replace(" ", "+")
-        url = "https://nominatim.openstreetmap.org/search?q="+location+"&format=json&polygon_geojson=1&addressdetails=1"
-        response = requests.get(
-            url)
-        resp_json_payload = response.json()
-        print(resp_json_payload)
-        resp_json_payload = dict(resp_json_payload[0])
-        print(resp_json_payload)
-        latitude  = resp_json_payload['lat']
-        longitude = resp_json_payload['lon']
-        return{'latitude':latitude, 'longitude': longitude}
-
-    def book_taxi(self, user_id, Origin, Destination, taxi_type):
-        path = "/api/rides/getaride"
-        origin_lat_lon = self.get_lat_long(Origin)
-        destination_lat_lon = self.get_lat_long(Destination)
-        data = {
-            "timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
-            "destLat": destination_lat_lon['latitude'],
-            "destLng": destination_lat_lon['longitude'],
-            "startLat": origin_lat_lon['latitude'],
-            "startLng": origin_lat_lon['longitude'],
-            "vehicle_type": taxi_type
-
-        }
-        r = requests.post(f'{self._base_url}{path}', json.dumps(data), headers=self._headers)
-        print(f'response1 - {r.text}')
-        if r.status_code == 200:
-            print(f'response - {r.text}')
-            return r.text
-        else:
-            print('Error while retrieving data')
-        return r.text
-
-    def book_taxi_confirm(self,  user_id, taxi_num, Origin, Destination, taxi_type,apiKey):
-        path = "/api/rides/confirmride"
-        origin_lat_lon = get_lat_long(Origin)
-        destination_lat_lon = get_lat_long(Destination)
-        data = {
-            "apiKey": apiKey,
-            "bookedTime": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
-            "destAddress": Destination,
-            "destLat": destination_lat_lon['latitude'],
-            "destLng": destination_lat_lon['longitude'],
-            "startAddress": Origin,
-            "startLat": origin_lat_lon['latitude'],
-            "startLng": origin_lat_lon['longitude'],
-            "vehicleNum": taxi_num,
-            "vehicle_type": taxi_type
-
-        }
-        res = requests.post(f'{self._base_url}{path}', json.dumps(data), headers=self._headers)
-        if res.status_code == 200:
             print(f'response - {res.text}')
             return res.text
         else:
             print('Error while retrieving data')
         return None
 
-    def start_ride(self, user_id, OTP, taxi_num, Origin, Destination, taxi_type,apiKey):
-        path = "/api/rides/updateride"
+    def get_lat_long(self, location):
+        location = location.replace(" ", "+")
+        url = "https://maps.googleapis.com/maps/api/geocode/json?address="+location+"&key="+self.GApiKey
+        response = requests.get(
+            url)
+        resp_json_payload = response.json()
+        print(resp_json_payload)
+        resp_json_payload = resp_json_payload['results']
+        resp_json_payload = resp_json_payload[0]
+        print(resp_json_payload)
+        latitude  = resp_json_payload['geometry']['location']['lat']
+        longitude = resp_json_payload['geometry']['location']['lng']
+        return{'latitude':latitude, 'longitude': longitude}
+
+    def get_location(self, latitude, longitude):
+        concat_lat_lng = ','.join([str(latitude) ,str(longitude)])
+        url = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+concat_lat_lng+"&sensor=false&key="+self.GApiKey
+        response = requests.get(
+            url)
+        resp_json_payload = response.json()
+        print(resp_json_payload)
+        resp_json_payload = resp_json_payload['results']
+        resp_json_payload = resp_json_payload[0]
+        print(resp_json_payload)
+        address = resp_json_payload['formatted_address']
+        return address
+    def get_distance(self, to_lat_lng, frm_lat_lng):
+
+        print(to_lat_lng, frm_lat_lng)
+        distance = geodesic((to_lat_lng['latitude'],to_lat_lng['longitude']), (frm_lat_lng['latitude'],frm_lat_lng['longitude'])).km
+        print(distance)
+        return distance
+
+    def book_taxi(self, user_id, Origin, Destination, taxi_type):
+        path = "/api/rides/getaride"
+        origin_lat_lon = self.get_lat_long(Origin)
+        destination_lat_lon = self.get_lat_long(Destination)
         data = {
-            "userId": user_id,
-            "timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
-            "vehicle_num": taxi_num,
-            "Destination": Destination,
-            "taxi_type": taxi_type,
-            "Origin": Origin,
-            "OTP": hashlib.sha256(OTP.encode()),
+            #"timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+            "dest_lat": destination_lat_lon['latitude'],
+            "dest_lng": destination_lat_lon['longitude'],
+            "start_lat": origin_lat_lon['latitude'],
+            "start_lng": origin_lat_lon['longitude'],
+            "vehicle_type": taxi_type
+
+        }
+        r = requests.post(f'{self._base_url}{path}', json.dumps(data), headers=self._headers)
+        print(r.status_code)
+        if r.status_code == 200:
+            return r.text
+        else:
+            print('Error while retrieving data')
+        return None
+
+    def book_taxi_confirm(self,  user_id, taxi_num, Origin, Destination, taxi_type,apiKey):
+        path = "/api/rides/confirmride"
+        origin_lat_lon = self.get_lat_long(Origin)
+        destination_lat_lon = self.get_lat_long(Destination)
+        data = {
             "apiKey": apiKey,
-            "startTime": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
-            "updateType": "startTime"
+            "booked_time": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+            "dest_address": Destination,
+            "dest_lat": destination_lat_lon['latitude'],
+            "dest_lng": destination_lat_lon['longitude'],
+            "start_address": Origin,
+            "start_lat": origin_lat_lon['latitude'],
+            "start_lng": origin_lat_lon['longitude'],
+            "vehicle_num": taxi_num,
+            "scheduled_time": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+            "vehicle_type": taxi_type
 
         }
         res = requests.post(f'{self._base_url}{path}', json.dumps(data), headers=self._headers)
         if res.status_code == 200:
-            print(f'Your ride started at  - {res.start_time}')
+            print(f'response - {res.text}')
+
             return res.text
         else:
             print('Error while retrieving data')
+        return None
+    def get_taxi_curr_location(self,  to_location,OTP,taxi_num):
+        path = "/api/rides/getridelocation"
+        tp_loc_lat_lon = self.get_lat_long(to_location)
+        data = {
+            "otpHash": str(hashlib.sha256( OTP.encode("utf-8") ).hexdigest()),
+            "vehicle_num": taxi_num
+
+        }
+        res = requests.post(f'{self._base_url}{path}', json.dumps(data), headers=self._headers)
+        if res.status_code == 200:
+            print(f'response - {res.text}')
+            print(res.text)
+            resp_json_payload = json.loads(res.text)
+            print(resp_json_payload['current_vehicle_location']['coordinates'])
+            lat_lng = resp_json_payload ['current_vehicle_location']['coordinates']
+            driver_address = self.get_location(lat_lng[0],lat_lng[1])
+            print(lat_lng)
+            print(tp_loc_lat_lon )
+            lat_lng= {'latitude':lat_lng[0], 'longitude': lat_lng[1]}
+            distance = self.get_distance(tp_loc_lat_lon,lat_lng)
+            return driver_address,distance
+        else:
+            print('Error while retrieving data')
+        return None
+
+    def start_ride(self, OTP, taxi_num, Origin, Destination,  apiKey):
+        path = "/api/rides/updateride"
+        origin_lat_lon = self.get_lat_long(Origin)
+        destination_lat_lon = self.get_lat_long(Destination)
+        data = {
+            "OTPHash": str(hashlib.sha256(OTP.encode("utf-8")).hexdigest()),
+            "apiKey": apiKey,
+            "dest_lat": destination_lat_lon['latitude'],
+            "dest_lng": destination_lat_lon['longitude'],
+            "start_lat": origin_lat_lon['latitude'],
+            "start_lng": origin_lat_lon['longitude'],
+            "start_time": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+            "update_type": "startTime",
+            "vehicle_num": taxi_num
+
+        }
+        res = requests.post(f'{self._base_url}{path}', json.dumps(data), headers=self._headers)
+        if res.status_code == 200:
+            print(f'Your ride started at  - {res.text}')
+            return res.text
+        else:
+            print(f'Error while retrieving data{res.text}')
         return None
 
     def end_ride(self, user_id, OTP, taxi_num, Origin, Destination, taxi_type, apiKey):
@@ -170,7 +217,7 @@ class User:
             "Destination": Destination,
             "taxi_type": taxi_type,
             "Origin": Origin,
-            "OTP": hashlib.sha256(OTP.encode()),
+            "OTP": str(hashlib.sha256( OTP.encode("utf-8") ).hexdigest()),
             "apiKey": apiKey,
             "startTime": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
             "updateType": "endTime"
@@ -179,7 +226,7 @@ class User:
         res = requests.post(f'{self._base_url}{path}', json.dumps(data), headers=self._headers)
         if res.status_code == 200:
             print(f'Your ride has ended  - {res.end_time}')
-            return res.text
+            return res.end_time
         else:
             print('Error while retrieving data')
         return None
